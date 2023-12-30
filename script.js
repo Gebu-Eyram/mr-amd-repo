6dca66814406e4ea616c86fddcc5bee29bdff17c
Heny <gebueyram18@gmail.com>
2023-12-30 14:41:57 +0000

    FINAL

--- script.js
+++ script.js
@@ -33,3 +33,31 @@ window.addEventListener("scroll",function(){
 
 menu.addEventListener("click",closemenu)
 body.addEventListener("click",closemenu)
+
+
+const popup = document.querySelector(".popup");
+
+const okayBtn = document.querySelector(".okay");
+const sendBtn = document.querySelector(".btn-submit");
+
+
+function showpopup(){
+    popup.style.bottom = "20%"
+}
+function closepopup(){
+    popup.style.bottom = "-50%"
+}
+
+okayBtn.addEventListener("click",closepopup)
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzmBj7oYyN_kcLW94A7cKw8clG_qEhaPbbYyDJc-Ayqzh-I_c25L6lFtj8urtBgGdHPHg/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+    showpopup();
+      
+  })
